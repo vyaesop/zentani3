4ef4a516f857bdc8089d95879cff1ec33a018ce1
vyaesop <100709552+vyaesop@users.noreply.github.com>
2026-03-07 11:54:57 +0300

    Fix Vercel build: install Python deps in virtualenv

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/sh
+set -eu
+
+# Use an isolated virtual environment to avoid PEP 668 system-package restrictions.
+python3 -m venv .vercel-venv
+. .vercel-venv/bin/activate
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput
