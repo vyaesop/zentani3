6f710567b8892762829eda00919a7eed12ade0e1
vyaesop <100709552+vyaesop@users.noreply.github.com>
2026-03-07 12:38:40 +0300

    Run Django migrations during Vercel build

--- build_files.sh
+++ build_files.sh
@@ -16,4 +16,5 @@ trap cleanup EXIT
 # Always run pip/manage.py through the venv interpreter (no shell activation required).
 "$VENV_DIR/bin/python" -m pip install --upgrade pip setuptools wheel
 "$VENV_DIR/bin/python" -m pip install -r requirements.txt
+"$VENV_DIR/bin/python" manage.py migrate --noinput
 "$VENV_DIR/bin/python" manage.py collectstatic --noinput
